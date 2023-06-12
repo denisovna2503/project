a46c9c00122d83e1953d1f8f963e5101a331ae8f
denisovna2503 <denisovna2503@mail.ru>
2023-06-12 21:07:36 +0300

    js

---
+++ javascript/second.js
@@ -0,0 +1,25 @@
+/*var n=123;//целое число
+n=12.345;//дробное число
+var b=1/0;//infinity
+var c="число"/2;//Nan
+
+var str="Мама мыла раму"
+var s='Одинарные кавычки также можно использовать'
+var letter='a';
+var checked=true;//истина
+var notchecked=false;//не совсем истина
+var user={
+    name:"Вася",
+    sex:"Мужской",
+    height: 180,
+    weight: 60.6
+};
+var age=null;
+age=10;
+var x;
+x=10;
+const bigint=1234567890n;
+alert(n)*/
+console.log(typeof'Student Lab');
+console.log(typeof true);
+console.log(typeof []);
